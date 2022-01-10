c185c7eb35baaaad88b739ef35cb69ac0da0400b
Franco-Global3 <franco@global3.cl>
2022-01-10 17:17:28 -0300

    Se agrega uso de autenticacion en configuracion de firebase y se crea metodo de registro con email y password

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,35 +1,81 @@
-import React from 'react'
+import React from 'react';
+
+
+import {createUserWithEmailAndPassword } from 'firebase/auth';
+import {auth} from '../firebase'
 
 const Login = () => {
 
     const [email, setEmail]=React.useState('');
     const [pass, setPass]=React.useState('');
+    const [error, setError]=React.useState(null);
+    const [esRegistro, setEsRegistro]=React.useState(true);
+
+    const register = async () => {
+        try {
+            const user = await createUserWithEmailAndPassword(auth, email, pass)    
+            console.log(user);
+        } catch (error) {
+            if(error.code === 'auth/invalid-email'){
+                setError("Email Ingresado no es válido");
+            }
+            if(error.code === 'auth/email-already-in-use'){
+                setError("Email Ingresado ya esta en uso");
+            }
+            /*console.log(error.code);
+            console.log(error.message);*/
+        }
+        
+    };
 
     const procesarDatos = e =>{
         e.preventDefault()
         if(!email.trim()){
-            console.log('Ingrese Email')
+            //console.log('Ingrese Email')
+            setError('Ingrese Email')
             return
         }
 
         if(!pass.trim()){
-            console.log('Ingrese Pass')
+            //console.log('Ingrese Pass')
+            setError('Ingrese Pass')
             return
         }
 
         if (pass.length<6) {
-            console.log('Pass debe tener 6 o más carácteres')
+            //console.log('Pass debe tener 6 o más carácteres')
+            setError('Pass debe tener 6 o más carácteres')
             return
         }
+
+        setError(null)
+
+        if(esRegistro){
+            
+            register()
+        }
+
     }
+
 
     return (
         <div className='mt-5'>
-            <h3 className="text-center">Registro de usuario</h3>
+            <h3 className="text-center">
+            {
+                esRegistro ? 'Registro de Usuario' : 'Acceso de Usuario'
+            }
+            </h3>
             <hr />
             <div className="row justify-content-center">
                 <div className="col-12 col-sm-8 col-md-6 col-xl-4">
                     <form onSubmit={procesarDatos}>
+                        {
+                            error && (
+                                <div className="alert alert-danger">
+                                    {error}
+                                </div>
+                            )
+                        }
                         <input type="email" 
                         className="form-control my-1"
                         placeholder='Ingrese un email' 
@@ -43,10 +89,19 @@ const Login = () => {
                         value={pass}/>
 
                         <div className="d-grid gap-2">
-                            <button className="btn btn-dark btn-lg my-1">Registrar</button>
+                            <button className="btn btn-dark btn-lg my-1" type='submit'>
+                            {
+                                esRegistro ? 'Registrarse' : 'Ingresar'
+                            }    
+                            </button>
                         </div>
                         <div className="d-grid gap-2">
-                            <button className="btn btn-info btn-sm my-1">Ya tienes Cuenta?</button>
+                            <button className="btn btn-info btn-sm my-1" type='button'
+                            onClick={()=> setEsRegistro(!esRegistro)}>
+                                {
+                                    esRegistro ? 'Ya tienes Cuenta?' : 'No tienes cuenta?'
+                                }
+                            </button>
                         </div>
                         
                     </form>
--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,19 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDNKp_AOIRf3xi_6VPAAoFn8fdOwwdOltc",
-    authDomain: "crud-udemy-react-d6582.firebaseapp.com",
-    projectId: "crud-udemy-react-d6582",
-    storageBucket: "crud-udemy-react-d6582.appspot.com",
-    messagingSenderId: "564317408342",
-    appId: "1:564317408342:web:519dee3564f3c6222f7ff7"
+  apiKey: "AIzaSyDNKp_AOIRf3xi_6VPAAoFn8fdOwwdOltc",
+  authDomain: "crud-udemy-react-d6582.firebaseapp.com",
+  projectId: "crud-udemy-react-d6582",
+  storageBucket: "crud-udemy-react-d6582.appspot.com",
+  messagingSenderId: "564317408342",
+  appId: "1:564317408342:web:519dee3564f3c6222f7ff7"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore()
-const auth = firebase.auth();
-
-export {db, auth}
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
